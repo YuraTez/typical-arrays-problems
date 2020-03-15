e6a83743bdf75f92584708e7207237d1c37f965b
YuraTez <52445676+YuraTez@users.noreply.github.com>
2020-03-15 23:34:40 +0300

    init:decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
-}
-
+    let minNam=0;
+    if(arguments.length===minNam || array.length===minNam){
+        return minNam;
+    }
+    for(let i=0; i<array.length; i++){
+        if(minNam > array[i]){
+            minNam = array[i];
+        }
+    }
+  return minNam;
+};
 exports.max = function max (array) {
-  return 0;
-}
+    let maxNam=0;
+
+    if(arguments.length===maxNam || array.length===maxNam){
+        return maxNam;
+    }
+    for(let i=0; i<array.length; i++){
+        if(maxNam< array[i]){
+            maxNam = array[i];
+        }
+    }
+  return maxNam;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    let midlNam=0;
+
+    if(arguments.length===midlNam || array.length===midlNam){
+        return midlNam;
+    }
+    for(let i=0; i<array.length; i++){
+            midlNam += array[i];
+    }
+    midlNam=midlNam/array.length;
+    return midlNam;
+};
